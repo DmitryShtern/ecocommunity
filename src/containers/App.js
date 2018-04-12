c56375a98341d1017ca71076ef6081b6ab2d3cc3
Unknown <demonshtern@gmail.com>
2018-04-12 15:18:36 +0300

    Удалил комменты для скрина

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,30 +6,7 @@ import { Switch, Route } from "react-router-dom"
 
 import { Header } from "../components"
 import { MainPage, NewsPage, ArticlePage, ServicesPage, GalleryPage, NewspaperPage } from "./"
-// import { changeActivePage } from "../actions"
 import { Link, browserHistory } from "react-router"
-
-// const App = ({ changeActivePage, activePage }) => (
-//   state = {users: []}
-
-//   componentDidMount() {
-//   fetch('/users')
-//   .then(res => res.json())
-//   .then(users => this.setState({ users }));
-//   }
-
-//   <div>
-//     <Header changeActivePage={changeActivePage} activePage={activePage} />
-//     <Switch>
-//       <Route exact path="/" component={MainPage} />
-//       <Route exact path="/news" component={NewsPage} />
-//       <Route path="/news/:id" component={ArticlePage} />
-//       <Route path="/services" component={ServicesPage} />
-//       <Route path="/gallery" component={GalleryPage} />
-//       <Route path="/newspaper" component={NewspaperPage} />
-//     </Switch>
-//   </div>
-// )
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import { render } from "react-dom"
 import { Provider } from "react-redux"
 import { ConnectedRouter } from "react-router-redux"
 import { default as configureStore, history } from "./store/configureStore"
-import { Router, BrowserRouter } from "react-router-dom"
 import { App } from "./containers"
-// import { syncHistoryWithStore } from "react-router-redux"
 
 const store = configureStore()
-// const history = syncHistoryWithStore(BrowserRouter, store)
 
 render(
   <Provider store={store}>
